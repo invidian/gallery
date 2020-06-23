cd6e1b8725fb64c06e3b3bf9b21bf24b39558206
Mateusz Gozdek <mgozdekof@gmail.com>
2020-06-23 19:48:34 +0200

    App.tsx: move main application part to separate function
    So we keep App() minimal.
    
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- App.tsx
+++ App.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+function Gallery() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!!</Text>
+    </View>
+  );
+}
+
 export default function App() {
   return (
     <SafeAreaProvider>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!!</Text>
-      </View>
+      <Gallery></Gallery>
     </SafeAreaProvider>
   );
 }
